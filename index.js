1ddb4865e7b39593b3697d6b2fc257164b930722
ELIUS <eliuswanyoro@gmail.com>
2022-08-15 15:01:35 +0300

    Added a few Changes

--- index.js
+++ index.js
@@ -5,15 +5,10 @@
  
    const multiply = num1 *num2;
 
-
-  
   const random = Number.isInteger(Math.random() * 1) + 5;
-
-
 
      const num3 = 22;
     const num4 = 6;
     const mod = num3 % num4;
-
 
     const max = Math.max(7, 20, 16, 4, 10);
